cfde7131cdb10ce397437b67c2ef5d49451f72fb
mmelrod <53342318+mmelrod@users.noreply.github.com>
2019-12-02 08:50:47 -0500

    Delete helper.js

--- helper.js
+++
@@ -1,9 +0,0 @@
-class Helper{
-  playPauseAndUpdate (song) {
-    player.playPause(song);
-    const totalTime = player.currentlyPlaying.duration;
-    $('#time-control .total-time').text( totalTime );
-  }
-}
-
-const helper = new Helper();
